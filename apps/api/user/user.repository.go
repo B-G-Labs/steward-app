76c2ed2135fb9a0acbcc2e9b90d68059f421f7a3
Gadiel Rangel <github@gadiel.dev>
2024-01-16 22:54:12 -0300

    first step of auth

--- apps/api/user/user.repository.go
+++ apps/api/user/user.repository.go
@@ -1,8 +1,10 @@
 package user
 
 import (
+	"api/utils"
 	"context"
 	"database/sql"
+	"log"
 
 	"github.com/uptrace/bun"
 )
@@ -23,14 +25,29 @@ func NewUserRepo(db *bun.DB) UserRepository {
 }
 
 func (r *UserRepository) CreateUser(user User, ctx context.Context) (sql.Result, error) {
+
+	p := &utils.ArgonParams{
+		Memory:      64 * 1024,
+		Iterations:  3,
+		Parallelism: 2,
+		SaltLength:  16,
+		KeyLength:   32,
+	}
+
+	hash, err := utils.GenerateFromPassword(user.Password, p)
+
+	if err != nil {
+		return nil, err
+	}
+
 	a, err := r.db.NewInsert().Model(&User{
 		ID:       user.ID,
 		Name:     user.Name,
-		Password: user.Password,
+		Password: hash,
 	}).Exec(ctx)
 
 	if err != nil {
-		return a, err
+		log.Fatal(err)
 	}
 
 	return a, nil
---
+++ apps/api/utils/crypto.go
@@ -0,0 +1,54 @@
+package utils
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"fmt"
+
+	"golang.org/x/crypto/argon2"
+)
+
+var (
+	ErrInvalidHash         = errors.New("the encoded hash is not in the correct format")
+	ErrIncompatibleVersion = errors.New("incompatible version of argon2")
+)
+
+type ArgonParams struct {
+	Memory      uint32
+	Iterations  uint32
+	Parallelism uint8
+	SaltLength  uint32
+	KeyLength   uint32
+}
+
+func GenerateFromPassword(password string, p *ArgonParams) (encodedHash string, err error) {
+	salt, err := generateRandomBytes(p.SaltLength)
+
+	if err != nil {
+		return "", err
+	}
+
+	hash := argon2.IDKey([]byte(password), salt, p.Iterations, p.Memory, p.Parallelism, p.KeyLength)
+
+	// Base64 encode the salt and hashed password.
+	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
+	b64Hash := base64.RawStdEncoding.EncodeToString(hash)
+
+	// Return a string using the standard encoded hash representation.
+	encodedHash = fmt.Sprintf("$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s", argon2.Version, p.Memory, p.Iterations, p.Parallelism, b64Salt, b64Hash)
+
+	return encodedHash, nil
+}
+
+func generateRandomBytes(n uint32) ([]byte, error) {
+	b := make([]byte, n)
+
+	_, err := rand.Read(b)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
