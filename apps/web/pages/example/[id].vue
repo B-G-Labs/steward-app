90cb1b2cf9a9707b7c3b1c724f20d3b04a6d11a5
Gadiel <github@gadiel.dev>
2024-02-12 20:02:56 -0300

    fixed errors

--- apps/web/pages/example/[id].vue
+++ apps/web/pages/example/[id].vue
@@ -1,30 +1,30 @@
 <template>
   <div>
     <div class="components-container">
-      <smexy-button size="big">Big</smexy-button>
-      <smexy-button size="medium">Medium</smexy-button>
-      <smexy-button size="small">Small</smexy-button>
+      <smexy-button>Big</smexy-button>
+      <smexy-button>Medium</smexy-button>
+      <smexy-button>Small</smexy-button>
     </div>
 
     <div class="components-container">
-      <smexy-button size="medium" btnStyle="fill">Fill</smexy-button>
-      <smexy-button size="medium">Outline</smexy-button>
-      <smexy-button size="medium">Flat</smexy-button>
+      <smexy-button btnStyle="fill">Fill</smexy-button>
+      <smexy-button>Outline</smexy-button>
+      <smexy-button>Flat</smexy-button>
     </div>
 
     <div>
       <smexy-button style="margin: 10px">Primary</smexy-button>
-      <smexy-button size="small">Primary small</smexy-button>
+      <smexy-button>Primary small</smexy-button>
     </div>
 
     <div>
-      <smexy-button style="margin: 10px" type="secondary">Secondary</smexy-button>
-      <smexy-button type="secondary" size="small">Secondary small</smexy-button>
+      <smexy-button style="margin: 10px">Secondary</smexy-button>
+      <smexy-button>Secondary small</smexy-button>
     </div>
 
     <div>
-      <smexy-button style="margin: 10px" type="primary" size="medium">butt</smexy-button>
-      <smexy-button size="small" class="primaryMedium">butter</smexy-button>
+      <smexy-button style="margin: 10px">butt</smexy-button>
+      <smexy-button class="primaryMedium">butter</smexy-button>
     </div>
   </div>
 </template>
@@ -37,9 +37,9 @@ const route = useRoute();
 
 <style scoped>
 .components-container {
-  margin: 1rem; 
-  display: flex; 
+  margin: 1rem;
+  display: flex;
   align-items: center;
-  gap: 1rem; 
+  gap: 1rem;
 }
 </style>
