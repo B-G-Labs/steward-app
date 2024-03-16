00e89d8d33880c07aed03af96f6af49e6a0bc9d5
Gadiel <github@gadiel.dev>
2024-03-15 21:13:55 -0300

    setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+chmod a+x install-docker.sh
+sh install-docker.sh
+
+# node and go managers
+curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s lts
+curl -sSL https://git.io/g-install | sh -s
+
+# install Caddy
+
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install caddy
